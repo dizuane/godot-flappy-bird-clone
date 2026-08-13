aac2cb94bbacec14f43f3fe26f690a7cecc31f1e
Duane Spainhour <duane.spainhour@gmail.com>
2026-08-12 22:33:07 -0400

    Collision detection, first pass

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -5,15 +5,25 @@ const SCROLL_SPEED: float = 120.0 # px/s
 
 
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var upper: Area2D = $Upper
+@onready var lower: Area2D = $Lower
 
 
 func _on_screen_exited() -> void:
 	queue_free()
 
 
+func _on_pipe_body_entered(body: Node2D) -> void:
+	print("_on_pipe_body_entered: %s %s" % [
+		name, body.name,
+	])
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible_on_screen_notifier_2d.connect("screen_exited", _on_screen_exited)
+	upper.body_entered.connect(_on_pipe_body_entered)
+	lower.body_entered.connect(_on_pipe_body_entered)
 
 
 func _physics_process(delta: float) -> void:
