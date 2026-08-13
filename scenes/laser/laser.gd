811d09d3d563a301eab0e37782fb58e110695d8e
Duane Spainhour <duane.spainhour@gmail.com>
2026-08-12 22:33:24 -0400

    Collision detection, added laser

---
+++ scenes/laser/laser.gd
@@ -0,0 +1,11 @@
+class_name Laser
+extends Area2D
+
+
+func _on_laser_entered(body: Node2D) -> void:
+	print("Point scored")
+
+
+
+func _ready() -> void:
+	body_entered.connect(_on_laser_entered, CONNECT_ONE_SHOT) # one shot to prevent accidental double scoring
