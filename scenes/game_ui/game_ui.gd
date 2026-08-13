de8ad8444c9a03986d4c1f38b17d465d24afa368
Duane Spainhour <duane.spainhour@gmail.com>
2026-08-13 00:34:37 -0400

    Added restart labe, input and game sounds

--- scenes/game_ui/game_ui.gd
+++ scenes/game_ui/game_ui.gd
@@ -3,10 +3,21 @@ extends Control
 
 
 @onready var game_over_label: Label = $MarginContainer/GameOverLabel
+@onready var press_jump_label: Label = $MarginContainer/PressJumpLabel
+@onready var sound: AudioStreamPlayer = $Sound
+@onready var label_change_timer: Timer = $LabelChangeTimer
+
+
+func _on_label_change_timer_timeout() -> void:
+	game_over_label.hide()
+	press_jump_label.show()
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("test"):
+	if (
+		event.is_action_pressed("test") or
+		(event.is_action_pressed("fly") and press_jump_label.visible)
+	):
 		GameManager.load_main_screen()
 
 
@@ -16,3 +27,6 @@ func _ready() -> void:
 
 func game_over() -> void:
 	game_over_label.show()
+	sound.play()
+	label_change_timer.timeout.connect(_on_label_change_timer_timeout, CONNECT_ONE_SHOT)
+	label_change_timer.start()
